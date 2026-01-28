5b9657d6408262e40eec557f013c41fe690ab7a3
Zuzana Stanickova <zuzana.stanickova@brighten.digital>
2026-01-28 13:38:49 +0100

    exercise 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,3 +1,11 @@
+const Part = (props) => {
+return (
+  <>
+  <p>{props.part} {props.exercises}</p>
+  </>
+)
+}
+
 const Header = (props) => {
   return (
     <>
@@ -9,15 +17,9 @@ const Header = (props) => {
 const Content = (props) => {
   return(
     <>
-    <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+    <Part part={props.part1} exercises={props.exercises1}/>
+    <Part part={props.part2} exercises={props.exercises2}/>
+    <Part part={props.part3} exercises={props.exercises3}/>
     </>
   )
 }
@@ -43,10 +45,10 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content 
+      <Content
         part1={part1} exercises1={exercises1}
         part2={part2} exercises2={exercises2}
-        part3={part3} exercises3={exercises3}/>
+        part3={part3} exercises3={exercises3} />
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
   )

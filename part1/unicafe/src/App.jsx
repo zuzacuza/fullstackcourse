a313213ebe802ba6960c5e66d43fd7237998c697
Zuzana Stanickova <zuzana.stanickova@brighten.digital>
2026-02-03 15:28:30 +0100

    finised 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -29,10 +29,25 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [sum, setSum] = useState(0)
+  const [positive, setPositive] = useState(0)
 
-  const handleGood = () => ( setGood(good+1) )
-  const handleNeutral = () => ( setNeutral(neutral+1) )
-  const handleBad = () => ( setBad(bad+1) )
+  const handleGood = () => {
+    setGood(good+1)
+    setTotal(total+1)
+    setPositive(positive+1)
+    setSum(sum+1)
+  }
+  const handleNeutral = () => { 
+    setNeutral(neutral+1)
+    setTotal(total+1)
+  }
+  const handleBad = () => {
+     setBad(bad+1)
+     setTotal(total+1)
+     setSum(sum-1)
+  }
 
   return (
     <div>
@@ -44,6 +59,9 @@ const App = () => {
       <Stats text ='good' count={good}/>
       <Stats text ='neutral' count={neutral}/>
       <Stats text ='bad' count={bad}/>
+      <Stats text ='total' count={total}/>
+      <Stats text='average' count={total === 0 ? 0: sum/total}/>
+      <Stats text ='positive' count={(total === 0 ? 0 : (positive / total) * 100) + '%'}/>
     </div>
   )
 }

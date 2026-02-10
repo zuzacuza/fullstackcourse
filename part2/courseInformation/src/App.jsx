6a67bf349ad814e24dd296bc79d689438974b1c1
Zuzana Stanickova <zuzana.stanickova@brighten.digital>
2026-02-10 11:31:24 +0100

    finished 2.2

--- part2/courseInformation/src/App.jsx
+++ part2/courseInformation/src/App.jsx
@@ -21,12 +21,22 @@ const Content = ({parts}) => {
     )
 }
 
+const Total = ({parts})=> {
+  const total = parts.reduce((sum, part) =>
+      sum + part.exercises, 0)
+  return(
+    <div>
+      Number of exercises {total}
+    </div>
+  )
+}
 
 const Course = ({course}) => {
   return (
     <div>
       <Header name = {course.name}/>
       <Content parts = {course.parts}/>
+      <Total parts = {course.parts}/>
     </div>
   )
 }
